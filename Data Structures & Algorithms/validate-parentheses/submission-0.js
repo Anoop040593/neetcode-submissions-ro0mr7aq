82370289741454ae4dd0d94812a35639076138ab
Anoop040593 <anuppemmaraju@outlook.com>
2026-04-06 23:20:55 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+
+        const map = {
+           ")" : "(",
+           "}" : "{",
+           "]" : "["
+        }
+
+        for(let char of s) {
+            if(map[char]) {
+                const top = stack.pop();
+                if(top !== map[char]) 
+                    return false;
+            } else {
+                stack.push(char);
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
